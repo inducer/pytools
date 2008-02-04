93e79713c42e59cbbf907ca5b1bb34528e471ffa
Andreas Kloeckner <inform@tiker.net>
2008-02-04 12:58:15 -0500

    Add outer_product to arithmetic_container.

--- src/arithmetic_container.py
+++ src/arithmetic_container.py
@@ -235,6 +235,14 @@ def work_with_arithmetic_containers(f, *args, **kwargs):
 
 
 
+def outer_product(al1, al2):
+    return ArithmeticListMatrix(
+            [[al1i*al2i for al2i in al2] for al1i in al1]
+            )
+
+
+
+
 class ArithmeticListMatrix:
     """A matrix type that operates on L{ArithmeticLists}."""
     def __init__(self, matrix):
