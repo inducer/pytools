59e7c17258448797c5e5d1309c4a2e963e031d19
Andreas Kloeckner <inform@tiker.net>
2008-02-07 13:15:50 -0500

    Add GvR's monkeypatch_class().

--- src/__init__.py
+++ src/__init__.py
@@ -347,10 +347,24 @@ class InfixOperator:
 
 
 def monkeypatch_method(cls):
+    # from GvR, http://mail.python.org/pipermail/python-dev/2008-January/076194.html
     def decorator(func):
         setattr(cls, func.__name__, func)
         return func
     return decorator
+
+
+
+
+def monkeypatch_class(name, bases, namespace):
+    # from GvR, http://mail.python.org/pipermail/python-dev/2008-January/076194.html
+
+    assert len(bases) == 1, "Exactly one base class required"
+    base = bases[0]
+    for name, value in namespace.iteritems():
+        if name != "__metaclass__":
+            setattr(base, name, value)
+    return base
 
 
 
