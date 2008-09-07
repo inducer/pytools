4c7d17ff32c78bbff3038eed314ee125f19fda87
Andreas Kloeckner <kloeckner@teramite.rice.edu>
2008-09-07 17:13:31 -0500

    Add a disk-backed dictionary.

---
+++ src/diskdict.py
@@ -0,0 +1,92 @@
+try:
+    import sqlite3 as sqlite
+except ImportError:
+    try:
+        from pysqlite2 import dbapi2 as sqlite
+    except ImportError:
+        raise ImportError, "could not find a usable version of sqlite."
+
+
+
+
+class DiskDict:
+    """Provides a disk-backed dictionary. Unlike L{shelve}, this class allows
+    arbitrary values for keys, at a slight performance penalty.
+    """
+    def __init__(self, name, version_base=None, dep_modules=[]):
+        import os
+        dbfilename = os.path.join(os.environ["HOME"], ".%s.pytools-dict" % name)
+        self.db_conn = sqlite.connect(dbfilename, timeout=30)
+
+        try:
+            self.db_conn.execute("select * from data;")
+        except sqlite.OperationalError:
+            self.db_conn.execute("""
+                  create table data (
+                    id integer primary key autoincrement,
+                    key_hash integer, 
+                    key_pickle blob, 
+                    version_hash integer, 
+                    version_pickle blob, 
+                    when_inserted timestamp default current_timestamp, 
+                    result_pickle blob)""")
+
+        def mtime(file):
+            if not isinstance(file, basestring):
+                # assume file names a module
+                file = file.__file__
+
+            import os
+            return os.stat(file).st_mtime
+
+        from cPickle import dumps
+        self.version = (version_base,) + tuple(
+            mtime(dm) for dm in dep_modules)
+        self.version_pickle = dumps(self.version)
+        self.version_hash = hash(self.version)
+
+        self.cache = {}
+
+    def __getitem__(self, key):
+        try:
+            return self.cache[key]
+        except KeyError:
+            from cPickle import loads
+            for key_pickle, version_pickle, result_pickle in self.db_conn.execute(
+                    "select key_pickle, version_pickle, result_pickle from data"
+                    " where key_hash = ? and version_hash = ?",
+                    (hash(key), self.version_hash)):
+                if loads(key_pickle) == key and loads(version_pickle) == self.version:
+                    result = loads(result_pickle) 
+                    self.cache[key] = result
+                    return result
+
+            raise KeyError(key)
+
+    def __delitem__(self, key):
+        if key in self.cache:
+            del self.cache[key]
+
+        from cPickle import loads
+        for item_id, arg_pickle, version_pickle in self.db_conn.execute(
+                "select key_pickle, version_pickle, result_pickle from data"
+                " where key_hash = ? and version_hash = ?",
+                (hash(key), self.version_hash)):
+            if loads(key_pickle) == key and loads(version_pickle) == self.version:
+                self.db_conn.execute("delete from data where id = ?", (item_id,))
+        self.db_conn.commit()
+             
+    def __setitem__(self, key, value):
+        del self[key]
+
+        self.cache[key] = value
+
+        from cPickle import dumps
+        self.db_conn.execute("insert into data"
+                " (key_hash, key_pickle, version_hash, version_pickle, result_pickle)"
+                " values (?,?,?,?,?)",
+                (hash(key), dumps(key), 
+                    self.version_hash, self.version_pickle,
+                    dumps(value)))
+        self.db_conn.commit()
+
