f87e50cb060ee72fc51a72b440d805b34b9d2bce
Andreas Kloeckner <inform@tiker.net>
2009-08-25 14:26:31 -0400

    Track py.test 1.0 marker changes.

--- pytools/test.py
+++ pytools/test.py
@@ -1,7 +1,9 @@
 try:
     from py.test import mark as mark_test
 except ImportError:
-    def mark_test(**kwargs):
-        def dec(f):
-            return f
-        return dec
+    class _Mark:
+        def __getattr__(self, name):
+            def dec(f):
+                return f
+            return dec
+    mark_test = _Mark()
