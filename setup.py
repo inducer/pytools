3f72757222dcf69721e604acdd7066cc2aef8ae0
Andreas Kloeckner <inform@tiker.net>
2013-06-07 10:33:27 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     from distutils.command.build_py import build_py
 
 setup(name="pytools",
-      version="2013.2",
+      version="2013.3",
       description="A collection of tools for Python",
       long_description="""
       Pytools is a big bag of things that are "missing" from the Python standard
