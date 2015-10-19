9092e14908ff36b9cd29a3a4be60ba7f0b1a8f3e
Andreas Kloeckner <inform@tiker.net>
2015-10-19 00:54:39 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name="pytools",
-      version="2015.1.3",
+      version="2015.1.4",
       description="A collection of tools for Python",
       long_description="""
       Pytools is a big bag of things that are "missing" from the Python standard
