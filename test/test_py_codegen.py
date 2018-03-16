5058655a326e868f58713da441618aab99aa84dd
Matt Wala <wala1@illinois.edu>
2018-03-16 15:13:44 -0500

    Fix a misleading assertion in test_pickling_with_module_import(). Only globals in the module are pickled

--- test/test_py_codegen.py
+++ test/test_py_codegen.py
@@ -8,14 +8,13 @@ import sys
 
 def test_pickling_with_module_import():
     cg = codegen.PythonCodeGenerator()
-    cg("import pytools.py_codegen")
+    cg("import pytools")
     cg("import math as m")
 
     import pickle
     mod = pickle.loads(pickle.dumps(cg.get_picklable_module()))
 
     assert mod.mod_globals["pytools"] is pytools
-    assert mod.mod_globals["pytools"].py_codegen is pytools.py_codegen
 
     import math
     assert mod.mod_globals["m"] is math
