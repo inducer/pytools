55a787d24f88c33b120caa315e536d9e005968ed
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-07-22 16:29:56 -0500

    add failing tests

--- test/test_graph_tools.py
+++ test/test_graph_tools.py
@@ -222,7 +222,7 @@ def test_induced_subgraph():
     assert subgraph == expected_subgraph
 
 
-def test_prioritzed_topological_sort():
+def test_prioritzed_topological_sort_examples():
 
     from pytools.graph import compute_topological_order
 
@@ -238,6 +238,62 @@ def test_prioritzed_topological_sort():
         return priorities[u]
 
     assert compute_topological_order(dag, key=key) == ['a', 'c', 'e', 'b', 'd']
+
+    priorities = {'a': 0, 'b': 5, 'c': 6, 'd': 7}
+    dag = {
+            'd': set('c'),
+            'b': set('a'),
+            'a': set(),
+            'c': set('a'),
+            }
+
+    assert compute_topological_order(dag, key=priorities.get) == ['d', 'c', 'b', 'a']
+
+
+def test_prioritzed_topological_sort():
+
+    import random
+    from pytools.graph import compute_topological_order
+    rng = random.Random(0)
+
+    def generate_random_graph(nnodes):
+        graph = dict((i, set()) for i in range(nnodes))
+        for i in range(nnodes):
+            # to avoid cycles only consider edges node_i->node_j where j > i.
+            for j in range(i+1, nnodes):
+                # Edge probability 4/n: Generates decently interesting inputs.
+                if rng.randint(0, nnodes - 1) <= 2:
+                    graph[i].add(j)
+        return graph
+
+    nnodes = rng.randint(40, 100)
+    rev_dep_graph = generate_random_graph(nnodes)
+    dep_graph = dict((i, set()) for i in range(nnodes))
+
+    for i in range(nnodes):
+        for rev_dep in rev_dep_graph[i]:
+            dep_graph[rev_dep].add(i)
+
+    priority = [rng.random() for _ in range(nnodes)]
+    topo_order = compute_topological_order(rev_dep_graph, key=priority.__getitem__)
+
+    for scheduled_node in topo_order:
+        nodes_with_no_deps = set(node for node, deps in dep_graph.items()
+                    if len(deps) == 0)
+
+        # check whether the order is a valid topological order
+        assert scheduled_node in nodes_with_no_deps
+        # check whether priorites are upheld
+        assert priority[scheduled_node] == max(priority[node] for node in
+                nodes_with_no_deps)
+
+        # 'scheduled_node' is scheduled => no longer a dependency
+        dep_graph.pop(scheduled_node)
+
+        for node, deps in dep_graph.items():
+            deps.discard(scheduled_node)
+
+    assert len(dep_graph) == 0
 
 
 if __name__ == "__main__":
