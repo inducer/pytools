e9d0504ca24290d2cc86ef14b2b7a62e4ec7b37c
Andreas Kloeckner <inform@tiker.net>
2020-09-29 18:35:12 -0500

    Put in place a shim for pytools.log

---
+++ pytools/log.py
@@ -0,0 +1,8 @@
+from warnings import warn
+
+warn("pytools.log was moved to https://github.com/illinois-ceesd/logpyle/. "
+        "I will try to import that for you. If the import fails, say "
+        "'pip install logpyle', and change your imports from 'pytools.log' "
+        "to 'logpyle'.", DeprecationWarning)
+
+from logpyle import *  # noqa
