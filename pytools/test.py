75c4fc05b7ac109e42b3683743ee10bc2c2cb49f
Nicholas Christensen <njchris2@illinois.edu>
2020-11-24 00:42:47 -0600

    add taggable function

---
+++ pytools/test.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+try:
+    from py.test import mark as mark_test  # pylint:disable=unused-import
+except ImportError:
+    class _Mark:
+        def __getattr__(self, name):
+            def dec(f):
+                return f
+            return dec
+    mark_test = _Mark()
