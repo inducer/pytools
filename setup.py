52599f81f2fbe4871f9e9529008b28f3a6b12090
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-01-04 14:11:08 -0600

    removes dep:Mako; makes 'lark' a soft dependency

--- setup.py
+++ setup.py
@@ -42,9 +42,11 @@ setup(name="pytools",
           "appdirs>=1.4.0",
           "numpy>=1.6.0",
           "dataclasses>=0.7;python_version<='3.6'",
-          "Mako",
-          "lark-parser",
           ],
+
+      extras_require={
+          "tag_parsing": ["lark-parser"],
+          },
 
       package_data={"pytools": ["py.typed"]},
 
