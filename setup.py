d0d518a0b78e4949c5522b3555845689c5cd3b72
Alexandru Fikl <alexfikl@gmail.com>
2021-11-01 14:43:03 -0500

    prefer platformdirs if available

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name="pytools",
       python_requires="~=3.6",
 
       install_requires=[
-          "appdirs>=1.4.0",
+          "platformdirs>=2.2.0",
           "numpy>=1.6.0",
           "dataclasses>=0.7;python_version<='3.6'"
           ],
