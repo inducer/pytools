c33785187ccb0e4722f952255ccb8fa97580fd3b
Matthias Diener <mdiener@illinois.edu>
2023-02-28 17:00:00 -0600

    init with https://code.activestate.com/recipes/523004-find-cyclical-references/

--- pytools/debug.py
+++ pytools/debug.py
@@ -139,6 +139,76 @@ def refdebug(obj, top_level=True, exclude=()):  # noqa: E501  pylint:disable=too
 # }}}
 
 
+# {{{ Find circular references
+
+# https://code.activestate.com/recipes/523004-find-cyclical-references/
+
+import gc
+from types import FrameType
+
+
+def print_cycles(objects, outstream=sys.stdout, show_progress=True):
+    """
+    objects:       A list of objects to find cycles in.  It is often useful
+                   to pass in gc.garbage to find the cycles that are
+                   preventing some objects from being garbage collected.
+    outstream:     The stream for output.
+    show_progress: If True, print the number of objects reached as they are
+                   found.
+    """
+    def print_path(path):
+        for i, step in enumerate(path):
+            # next_obj "wraps around"
+            next_obj = path[(i + 1) % len(path)]
+
+            outstream.write("   %s -- " % str(type(step)))
+            if isinstance(step, dict):
+                for key, val in step.items():
+                    if val is next_obj:
+                        outstream.write("[%s]" % repr(key))
+                        break
+                    if key is next_obj:
+                        outstream.write("[key] = %s" % repr(val))
+                        break
+            elif isinstance(step, list):
+                outstream.write("[%d]" % step.index(next_obj))
+            elif isinstance(step, tuple):
+                outstream.write("[%d]" % step.index(next_obj))
+            else:
+                outstream.write(repr(step))
+            outstream.write(" ->\n")
+        outstream.write("\n")
+
+    def recurse(obj, start, all_objs, current_path):
+        if show_progress:
+            outstream.write("%d\r" % len(all_objs))
+
+        all_objs.add(id(obj))
+
+        referents = gc.get_referents(obj)
+        for referent in referents:
+            # If we've found our way back to the start, this is
+            # a cycle, so print it out
+            if referent is start:
+                print_path(current_path)
+
+            # Don't go back through the original list of objects, or
+            # through temporary references to the object, since those
+            # are just an artifact of the cycle detector itself.
+            elif referent is objects or isinstance(referent, FrameType):
+                continue
+
+            # We haven't seen this object before, so recurse
+            elif id(referent) not in all_objs:
+                recurse(referent, start, all_objs, current_path + [obj])
+
+    for obj in objects:
+        outstream.write(f"Examining: {obj}\n")
+        recurse(obj, obj, set(), [])
+
+# }}}
+
+
 # {{{ interactive shell
 
 def get_shell_hist_filename():
