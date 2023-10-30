b1eeb3d9980e7a7d7bbc94afe5558712f04c81ae
Andreas Kloeckner <inform@tiker.net>
2023-10-30 13:38:56 -0500

    Move tests under package directory

--- pytools/log.py
+++
@@ -1,9 +0,0 @@
-from warnings import warn
-
-
-warn("pytools.log was moved to https://github.com/illinois-ceesd/logpyle/. "
-        "I will try to import that for you. If the import fails, say "
-        "'pip install logpyle', and change your imports from 'pytools.log' "
-        "to 'logpyle'.", DeprecationWarning)
-
-from logpyle import *  # noqa  # pylint: disable=import-error
