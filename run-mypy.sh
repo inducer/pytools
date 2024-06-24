87cc6bf298bcc1d5b8cc32a2947b7f8477129777
Alexandru Fikl <alexfikl@gmail.com>
2024-06-24 10:38:47 +0300

    mypy: remove --follow-imports=skip checks

--- run-mypy.sh
+++ run-mypy.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 
-set -ex
-
-mypy --show-error-codes pytools
-
-mypy --strict --follow-imports=skip pytools/datatable.py pytools/persistent_dict.py
+python -m mypy --show-error-codes pytools
